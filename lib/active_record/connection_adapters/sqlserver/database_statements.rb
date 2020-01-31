eb78fe27e27880d5b682c651f92f4320ac4cf110
Ryan Veazey <ryan.veazey@gmail.com>
2020-01-31 14:48:02 -0600

    putting back in odbc support

--- lib/active_record/connection_adapters/sqlserver/database_statements.rb
+++ lib/active_record/connection_adapters/sqlserver/database_statements.rb
@@ -108,6 +108,18 @@ module ActiveRecord
                 yield(r) if block_given?
               end
               result.each.map { |row| row.is_a?(Hash) ? row.with_indifferent_access : row }
+            when :odbc
+              results = []
+              raw_connection_run(sql) do |handle|
+                get_rows = lambda do
+                  rows = handle_to_names_and_values handle, fetch: :all
+                  rows.each_with_index { |r, i| rows[i] = r.with_indifferent_access }
+                  results << rows
+                end
+                get_rows.call
+                get_rows.call while handle_more_results?(handle)
+              end
+              results.many? ? results : results.first
             end
           end
         end
@@ -272,6 +284,8 @@ module ActiveRecord
           case @connection_options[:mode]
           when :dblib
             @connection.execute(sql).do
+          when :odbc
+            @connection.do(sql)
           end
         ensure
           @update_sql = false
@@ -318,12 +332,16 @@ module ActiveRecord
           case @connection_options[:mode]
           when :dblib
             @connection.execute(sql)
+          when :odbc
+            block_given? ? @connection.run_block(sql) { |handle| yield(handle) } : @connection.run(sql)
           end
         end
 
         def handle_more_results?(handle)
           case @connection_options[:mode]
           when :dblib
+          when :odbc
+            handle.handle_more_results?
           end
         end
 
@@ -331,6 +349,8 @@ module ActiveRecord
           case @connection_options[:mode]
           when :dblib
             handle_to_names_and_values_dblib(handle, options)
+          when :odbc
+            handle_to_names_and_values_odbc(handle, options)
           end
         end
 
@@ -344,10 +364,28 @@ module ActiveRecord
           options[:ar_result] ? ActiveRecord::Result.new(columns, results) : results
         end
 
+        def handle_to_names_and_values_odbc(handle, options = {})
+          @connection.use_utc = ActiveRecord::Base.default_timezone == :utc
+          if options[:ar_result]
+            columns = lowercase_schema_reflection ? handle.columns(true).map { |c| c.name.downcase } : handle.columns(true).map { |c| c.name }
+            rows = handle.fetch_all || []
+            ActiveRecord::Result.new(columns, rows)
+          else
+            case options[:fetch]
+            when :all
+              handle.each_hash || []
+            when :rows
+              handle.fetch_all || []
+            end
+          end
+        end
+
         def finish_statement_handle(handle)
           case @connection_options[:mode]
           when :dblib
             handle.cancel if handle
+          when :odbc
+            handle.drop if handle && handle.respond_to?(:drop) && !handle.finished?
           end
           handle
         end
--- lib/active_record/connection_adapters/sqlserver_adapter.rb
+++ lib/active_record/connection_adapters/sqlserver_adapter.rb
@@ -166,6 +166,8 @@ module ActiveRecord
         case @connection_options[:mode]
         when :dblib
           @connection.close rescue nil
+        when :odbc
+          @connection.disconnect rescue nil
         end
         @connection = nil
         @spid = nil
@@ -353,6 +355,8 @@ module ActiveRecord
         @connection = case config[:mode]
                       when :dblib
                         dblib_connect(config)
+                      when :odbc
+                        odbc_connect(config)
                       end
         @spid = _raw_select('SELECT @@SPID', fetch: :rows).first.first
         @version_year = version_year
@@ -362,6 +366,7 @@ module ActiveRecord
       def connection_errors
         @connection_errors ||= [].tap do |errors|
           errors << TinyTds::Error if defined?(TinyTds::Error)
+          errors << ODBC::Error if defined?(ODBC::Error)
         end
       end
 
@@ -394,6 +399,25 @@ module ActiveRecord
           client.execute('SET IMPLICIT_TRANSACTIONS OFF').do
           client.execute('SET TEXTSIZE 2147483647').do
           client.execute('SET CONCAT_NULL_YIELDS_NULL ON').do
+        end
+      end
+
+      def odbc_connect(config)
+        if config[:dsn].include?(';')
+          driver = ODBC::Driver.new.tap do |d|
+            d.name = config[:dsn_name] || 'Driver1'
+            d.attrs = config[:dsn].split(';').map { |atr| atr.split('=') }.reject { |kv| kv.size != 2 }.reduce({}) { |a, e| k, v = e ; a[k] = v ; a }
+          end
+          ODBC::Database.new.drvconnect(driver)
+        else
+          ODBC.connect config[:dsn], config[:username], config[:password]
+        end.tap do |c|
+          begin
+            c.use_time = true
+            c.use_utc = ActiveRecord::Base.default_timezone == :utc
+          rescue Exception
+            warn 'Ruby ODBC v0.99992 or higher is required.'
+          end
         end
       end
 
